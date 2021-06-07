f4a200575efde0c3b03d1efd24b691f04878b5bd
Omar Cossio <omar119mce@gmail.com>
2021-06-07 02:27:50 -0400

    edited challenge

--- M1-Challenge-Cossio-Omar/Challenge2/src/main/java/com/company/CalculatorObject.java
+++ M1-Challenge-Cossio-Omar/Challenge2/src/main/java/com/company/CalculatorObject.java
@@ -1,36 +1,56 @@
 package com.company;
 
 public class CalculatorObject {
+    public int answer;
+    double answerB;
+
 
     public int addInt(int a, int b) {
+        answer = a + b;
+        System.out.println(a + " + " + b + " = " + answer);
         return a + b;
     }
 
     public int subtractInt(int a, int b) {
+        answer = a- b;
+        System.out.println(a + " - " + b + " = " + answer);
         return a - b;
     }
 
+
     public int multiplyInt(int a, int b) {
+        answer = a * b;
+        System.out.println(a + " * " + b + " = " + answer);
         return a * b;
     }
 
     public int divideInt(int a, int b) {
+        answer = a / b;
+        System.out.println(a + " / " + b + " = " + answer);
         return a / b;
     }
 
     public double addDouble(double a, double b) {
+        answerB = a + b;
+        System.out.println(a + " + " + b + " = " + answerB);
         return a + b;
     }
 
     public double subtractDouble(double a, double b) {
+        answerB = a - b;
+        System.out.println(a + " - " + b + " = " + answerB);
         return a - b;
     }
 
     public double multiplyDouble(double a, double b) {
+        answerB = a * b;
+        System.out.println(a + " * " + b + " = " + answerB);
         return a * b;
     }
 
     public double divideDouble(double a, double b) {
+        answerB = a / b;
+        System.out.println(a + " / " + b + " = " + answerB);
         return a / b;
     }
 
